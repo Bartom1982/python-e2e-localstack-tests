46c5afaabca4a831c45c7c6edb4514eedcafc03a
tomasz.bargiela <bartom82@interia.pl>
2024-06-07 16:48:08 +0200

    add basic test

---
+++ api/test_basic_api.py
@@ -0,0 +1,25 @@
+import requests
+import allure
+
+
+@allure.feature('IP API')
+@allure.story('Basic API tests')
+@allure.title('My IP request status is 200')
+def test_my_ip_request_status_is200():
+    url = "https://api.ipify.org/?format=json"
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_my_ip_request_has_a_body():
+    url = "https://api.ipify.org/?format=json"
+    response = requests.get(url)
+    data = response.json()
+    assert data is not None
+    assert data['ip'] is not None
+
+
+def test_my_ip_has_json_header():
+    url = "https://api.ipify.org/?format=json"
+    response = requests.get(url)
+    assert response.headers["Content-Type"] == "application/json"
